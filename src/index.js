37f486273a19232d6a69bb3b62e7b718b476d94f
AliaxandraKasich <Sane4ka022@mail.ru>
2018-02-22 15:21:01 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var count5 = 0;
+
+  var tempNumber5 = number;
+  
+  var temp = number;
+  while(temp >= 1){
+    temp = Math.trunc(tempNumber5/5);
+    count5 = count5 + temp;
+    tempNumber5 = tempNumber5/5;
+  }
+  return count5;
+  
+ 
 }
